e336a26edd68c524c4d7e1e484b2a2b2887292ea
Thoinguyen96 <vanthoiqng@gmail.com>
2023-08-07 15:26:04 +0700

    #16. React Component - Làm Quen với Cách Viết Component

---
+++ src/component/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default class MyComponent extends React.Component {
+    render() {
+        return <div>My first component</div>;
+    }
+}
+// export default function MyComponent() {
+//     return <div>My first component </div>;
+// }
