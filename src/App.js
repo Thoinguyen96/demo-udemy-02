2bd1e7c5cec03b3c7ff6202c5e98196de7c4e7ad
Thoinguyen96 <vanthoiqng@gmail.com>
2023-08-19 15:07:55 +0700

    test redux

--- src/App.js
+++ src/App.js
@@ -8,25 +8,21 @@ const App = () => {
     const dispatch = useDispatch();
 
     return (
-        <div>
-            hello word
-            <MyComponent />
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div>Count = {count}</div>
+                <button onClick={() => dispatch(increaseCounter())}>
+                    Increase
+                </button>
+                <button onClick={() => dispatch(decreaseCounter())}>
+                    Decrease
+                </button>
+            </header>
         </div>
-        // <div className="App">
-        //     <header className="App-header">
-        //         <img src={logo} className="App-logo" alt="logo" />
-        //         <p>
-        //             Edit <code>src/App.js</code> and save to reload.
-        //         </p>
-        //         <div>Count = {count}</div>
-        //         <button onClick={() => dispatch(increaseCounter())}>
-        //             Increase
-        //         </button>
-        //         <button onClick={() => dispatch(decreaseCounter())}>
-        //             Decrease
-        //         </button>
-        //     </header>
-        // </div>
     );
 };
 
